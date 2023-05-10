b800e2096050fea4019525fa59147e5d20d602f7
JeffYang1030 <132439822+JeffYang1030@users.noreply.github.com>
2023-05-10 13:05:49 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,5 +9,6 @@
 
 const urls = [
 	'https://docs.google.com/forms/d/e/1FAIpQLSe2G1Amj8ObYDqQRjHiaCTm37gNvtTASVXyJ4aCf1dqQeG1CQ/viewform',
-	'https://docs.google.com/forms/d/e/1FAIpQLScCC5B8dLqrjzIHWijYFW0u5tTtpYL0faF59dH10xLfgjihWg/viewform?usp=share_link'
+	'https://docs.google.com/forms/d/e/1FAIpQLScCC5B8dLqrjzIHWijYFW0u5tTtpYL0faF59dH10xLfgjihWg/viewform?usp=share_link',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdS1oNacyrmStwbe9rzc0UxzCxe6nkvNcgwt87oLm3bZk2azw/viewform?usp=share_link'
 ];
